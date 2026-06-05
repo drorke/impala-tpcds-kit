382426fdcbd038279f21a976fe0a3b180a019f38
David Rorke <drorke@cloudera.com>
2026-06-05 16:40:10 -0700

    Add starrocks DDL scripts

---
+++ scripts/analyze-starrocks.sql
@@ -0,0 +1,28 @@
+
+
+use starrocks_iceberg.tpcds_3000_iceberg_parquet_fix;
+
+analyze full table call_center;
+analyze full table catalog_page;
+analyze full table customer;
+analyze full table customer_address;
+analyze full table customer_demographics;
+analyze full table date_dim;
+analyze full table household_demographics;
+analyze full table income_band;
+analyze full table item;
+analyze full table promotion;
+analyze full table reason;
+analyze full table ship_mode;
+analyze full table store;
+analyze full table time_dim;
+analyze full table warehouse;
+analyze full table web_page;
+analyze full table web_site;
+analyze full table catalog_returns;
+analyze full table catalog_sales;
+analyze full table inventory;
+analyze full table store_returns;
+analyze full table store_sales;
+analyze full table web_returns;
+analyze full table web_sales;
